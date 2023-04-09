8464e59f23830852addf6f4a31ca2770278fd2eb
AngelNBazan <angelnaimbazan@gmail.com>
2023-04-08 22:17:48 -0700

    F1 App

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TableDisplay from "~/components/Table/TableDisplay";
+
+export default function Home() {
+  return (
+    <div>
+      <TableDisplay />
+    </div>
+  );
+}
